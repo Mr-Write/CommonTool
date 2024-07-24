7beb3bf446dcdcc0bada489651ed7d1a96f034cc
谢添 <tian.xie2@mycos.com>
2024-07-24 21:36:58 +0800

    修改 ConfigCommon.DefaultOutputDir

--- SqlInfoGen.Cons/Common/ConfigCommon.cs
+++ SqlInfoGen.Cons/Common/ConfigCommon.cs
@@ -7,7 +7,7 @@ public static class ConfigCommon
     public static readonly string ConnectionStringsName = "connectionStrings";
     public static readonly string ReadDbConfigPathsName = "readDbConfigPaths";
     
-    public static readonly string DefaultOutputDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}{Path.DirectorySeparatorChar}TableGem";
+    public static readonly string DefaultOutputDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}{Path.DirectorySeparatorChar}TableGen";
     public static string DefaultOutputFileNamePrefix => $"{DateTime.Now:yyyyMMdd-HHmmss}_";
     public static readonly string  DefaultOutputFileNameSuffix = "md";
     
